b97d3138fa7141f051f42f0d63ac41c1e67d6f53
Dhandi Adam <dandiadam211@gmail.com>
2024-06-08 02:52:11 +0700

    Belajar Tailwindcss part1

---
+++ part1-tailwindcss/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ['./public/**/*.{html, js}', '/src/**/*.{html, js}'], Kalau misal mau liat folder 
+  // Tambahkan koma , '/src/**/*.{html, js}'
+  content: ['./public/**/*.{html, js}', '/src/**/*.{html, js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
